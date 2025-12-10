b6b357517c439860f87b4ccbb4f298aed58b94ad
Harinder Singh <harinders9230@gmail.com>
2025-12-10 19:31:39 +0530

    final

--- press.js
+++ press.js
@@ -1,11 +1,14 @@
 function press(){
     var a=document.getElementsByTagName('body')[0];
     var c=getComputedStyle(a).color;
-    if(c!='black'){
+    if(c.includes("rgb(0, 0, 0)")){
         a.style.background='black'
         a.style.color='white'
         document.getElementsByTagName('img')[1].src='images/download.png'
-        document.getElementsByTagName('ul')[0].style.color='white'
+        document.getElementsByTagName('img')[0].src='images/logo/fav-icon/bg removed.png'
+        document.getElementById('img').src='images/logo/fav-icon/icon.png'
+        // document.getElementById('li').style.background='white'
+        document.getElementById('to-top').style.background='white'
 
     } else {
         a.style.background='white'
